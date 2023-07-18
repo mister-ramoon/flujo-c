aff0371b3f1ecad79e61c5126428d0bd839c894d
ramonruizdev <ramonruizrc@gmail.com>
2023-07-17 22:14:48 -0600

    Expresión goto

---
+++ goto.c
@@ -0,0 +1,13 @@
+for (size_t i = 0; i < count; i++)
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            if(panic)
+                goto errorHandler;
+        }
+    }
+}
+
+errorHandler: // Todo el código para manejar el error
